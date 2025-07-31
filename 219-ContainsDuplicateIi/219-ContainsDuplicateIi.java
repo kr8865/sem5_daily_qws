d8498a03053d147d3588bc38650acaaa32c69eef
kratika agrawal <133734949+kr8865@users.noreply.github.com>
2025-07-31 14:06:45 +0530

    Create 219-ContainsDuplicateIi

---
+++ 219-ContainsDuplicateIi/219-ContainsDuplicateIi.java
@@ -0,0 +1,13 @@
+// Last updated: 7/31/2025, 2:06:46 PM
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+       for(int i=0;i<nums.length;i++){
+        for(int j=i+1;j<=i+k&&j<nums.length;j++){
+            if(nums[i]==nums[j]){
+                return true;
+            }
+        }
+        }
+       return false; 
+    }
+}

4e09797942ccf94074f8e1cb61dec5b639829323
kratika agrawal <133734949+kr8865@users.noreply.github.com>
2025-08-02 12:59:52 +0530

    Create 410-SplitArrayLargestSum

---
+++ 410-SplitArrayLargestSum/version/java/410-SplitArrayLargestSum_20250802_125952.java
@@ -0,0 +1,43 @@
+// Last updated: 8/2/2025, 12:59:52 PM
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int max=-1;
+        int s=0;
+        int ans=-1;
+        for(int i=0;i<nums.length;i++){
+            if(max<nums[i]){
+                max=nums[i];
+            }
+            s+=nums[i];
+        }
+        int lo=max;
+        int hi=s;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(canadd(nums,mid,k)){
+                ans=mid;
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        return ans;
+    }
+    public static boolean canadd(int[] nums,int mid,int k){
+        int l=1;
+        int s=0;
+        for(int i=0;i<nums.length;i++){
+            if(s+nums[i]<=mid){
+                s+=nums[i];
+
+            }
+            else{
+                l++;
+                s=nums[i];
+            }
+
+        }
+        return l<=k;
+    }
+}

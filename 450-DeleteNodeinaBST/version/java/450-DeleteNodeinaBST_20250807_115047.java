e8c707f966c7e9f7629575179649384d2977f996
kratika agrawal <133734949+kr8865@users.noreply.github.com>
2025-08-07 11:50:47 +0530

    Create 450-DeleteNodeinaBST

---
+++ 450-DeleteNodeinaBST/version/java/450-DeleteNodeinaBST_20250807_115047.java
@@ -0,0 +1,64 @@
+// Last updated: 8/7/2025, 11:50:47 AM
+// delete_node
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        return delete(root,key);
+        
+        
+    }
+    public static int maxa(TreeNode root) {
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        }
+        int da = maxa(root.right);
+        return Math.max(da, root.val);
+
+    }
+    public static TreeNode delete(TreeNode root,int key){
+        if(root==null){
+            return null;
+        }
+        if(root.val<key){
+            root.right=delete(root.right,key);
+        }
+        else if(root.val>key){
+            root.left=delete(root.left,key);
+        }
+        else{
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+
+            } else {
+                int maxv=maxa(root.left);
+                root.left=delete(root.left, maxv);
+                root.val=maxv;
+
+            }
+            
+           
+            
+        }
+        return root;
+        
+
+    }
+     
+
+}

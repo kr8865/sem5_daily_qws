a8c27d0c2ae4640cb2c13de83322146811ce9433
kratika agrawal <133734949+kr8865@users.noreply.github.com>
2025-08-08 17:02:18 +0530

    Create 783-MinimumDistanceBetweenBSTNodes

---
+++ 783-MinimumDistanceBetweenBSTNodes/version/java/783-MinimumDistanceBetweenBSTNodes_20250808_170218.java
@@ -0,0 +1,39 @@
+// Last updated: 8/8/2025, 5:02:18 PM
+// min_diff
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Integer prev = null;
+    int minDiff = Integer.MAX_VALUE;
+
+    public int minDiffInBST(TreeNode root) {
+        inorder(root);
+        return minDiff;
+    }
+
+    private void inorder(TreeNode node) {
+        if (node == null) return;
+
+        inorder(node.left);
+
+        if (prev != null) {
+            minDiff = Math.min(minDiff, node.val - prev);
+        }
+        prev = node.val;
+
+        inorder(node.right);
+    }
+}

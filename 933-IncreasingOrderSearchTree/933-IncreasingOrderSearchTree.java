5c0f9d9d9019c555e69dafffb23c2cf683a0aee9
kratika agrawal <133734949+kr8865@users.noreply.github.com>
2025-08-17 10:25:38 +0530

    Create 933-IncreasingOrderSearchTree

---
+++ 933-IncreasingOrderSearchTree/933-IncreasingOrderSearchTree.java
@@ -0,0 +1,43 @@
+// Last updated: 8/17/2025, 10:27:20 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode increasingBST(TreeNode root) {
+        ArrayList<Integer> ll=new ArrayList<>();
+        inorder(root,ll);
+        TreeNode rooot=buildtree(ll,0);
+        return rooot;
+        
+    }
+    public static void inorder(TreeNode root,ArrayList<Integer> ll){
+        if(root==null){
+            return;
+        }
+        inorder(root.left,ll);
+        ll.add(root.val);
+        inorder(root.right,ll);
+    }
+    
+    public static TreeNode buildtree(ArrayList<Integer> ll,int i){
+        if(i>=ll.size()){
+            return null;
+        }
+        TreeNode node=new TreeNode(ll.get(i));
+        node.right=buildtree(ll,i+1);
+        return node;
+        
+    }
+}

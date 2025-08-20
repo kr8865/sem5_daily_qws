ed0db72aca267afde644ef3b618d356423d341a2
kratika agrawal <133734949+kr8865@users.noreply.github.com>
2025-08-20 10:12:47 +0530

    Create 103-BinaryTreeZigzagLevelOrderTraversal

---
+++ 103-BinaryTreeZigzagLevelOrderTraversal/version/java/103-BinaryTreeZigzagLevelOrderTraversal_20250820_101246.java
@@ -0,0 +1,57 @@
+// Last updated: 8/20/2025, 10:12:46 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        boolean leftToRight = true;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            LinkedList<Integer> row = new LinkedList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+
+                // Add value in correct order
+                if (leftToRight) {
+                    row.addLast(node.val);   // left to right
+                } else {
+                    row.addFirst(node.val);  // right to left
+                }
+
+                // Enqueue children
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+
+            // Add current level to result
+            result.add(row);
+
+            // Switch direction
+            leftToRight = !leftToRight;
+        }
+
+        return result;
+    }
+}

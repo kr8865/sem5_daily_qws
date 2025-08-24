5a31ef7ea4581870939a0c43cc6bd150b0d6d5c0
kratika agrawal <133734949+kr8865@users.noreply.github.com>
2025-08-24 12:41:04 +0530

    Create 1046-LastStoneWeight

---
+++ 1046-LastStoneWeight/version/java/1046-LastStoneWeight_20250824_124109.java
@@ -0,0 +1,26 @@
+// Last updated: 8/24/2025, 12:41:09 PM
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<stones.length;i++){
+            pq.add(stones[i]);
+
+        }
+        while(pq.size()>1){
+            int y=pq.remove();
+            int x=pq.remove();
+            if(y!=x){
+                int ny=y-x;
+                pq.add(ny);
+
+            }
+        }
+        if(pq.peek()==null){
+            return 0;
+        }
+        return pq.peek();
+       
+    
+        
+    }
+}

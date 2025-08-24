4376a72e55922fdff41740394ebad0cb5d71a98a
kratika agrawal <133734949+kr8865@users.noreply.github.com>
2025-08-24 12:16:42 +0530

    Create 1337-TheKWeakestRowsinaMatrix

---
+++ 1337-TheKWeakestRowsinaMatrix/version/java/1337-TheKWeakestRowsinaMatrix_20250824_121646.java
@@ -0,0 +1,44 @@
+// Last updated: 8/24/2025, 12:16:46 PM
+class Solution {
+    public static class soldier implements Comparable<soldier>{
+        int sol;
+        int i;
+        soldier(int sol,int i){
+            this.sol=sol;
+            this.i=i;
+        }
+        @Override
+        public int compareTo(soldier s2){
+
+            if(this.sol==s2.sol){
+                return this.i-s2.i;
+            }
+            else{
+                return this.sol-s2.sol;
+            }
+        }
+    }
+    public int[] kWeakestRows(int[][] mat, int k) {
+        PriorityQueue<soldier> pq=new PriorityQueue<>();
+        for(int i=0;i<mat.length;i++){
+            int cs=0;
+            for(int j=0;j<mat[0].length;j++){
+                if(mat[i][j]==1){
+                    cs+=mat[i][j];
+                }
+            }
+            pq.add(new soldier(cs,i));
+            
+
+
+        }
+        int[] ar=new int[k];
+        
+        for(int i=0;i<k;i++){
+            ar[i]=pq.remove().i;
+        }
+        return ar;
+        
+        
+    }
+}

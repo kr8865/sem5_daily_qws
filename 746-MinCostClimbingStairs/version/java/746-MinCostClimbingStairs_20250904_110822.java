daf2f0ef1ba3ada861ca70a69f03eb1e34c38262
kratika agrawal <133734949+kr8865@users.noreply.github.com>
2025-09-04 11:07:57 +0530

    Create 746-MinCostClimbingStairs

---
+++ 746-MinCostClimbingStairs/version/java/746-MinCostClimbingStairs_20250904_110822.java
@@ -0,0 +1,21 @@
+// Last updated: 9/4/2025, 11:08:22 AM
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length];
+        Arrays.fill(dp,-1);
+        int zeroth = climbing(cost,0,dp);
+        int first = climbing(cost,1,dp);
+        return Math.min(zeroth,first);
+    }
+    public int climbing(int[] arr, int i, int[] dp){
+        if(i>=arr.length){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int f = climbing(arr, i+1,dp);
+        int s = climbing(arr, i+2,dp);
+        return dp[i] = Math.min(f,s)+arr[i];
+    }
+}

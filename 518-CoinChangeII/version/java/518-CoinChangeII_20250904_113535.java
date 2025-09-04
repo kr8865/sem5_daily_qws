c18b8b540253b20320f010d4203d248a0fcc6470
kratika agrawal <133734949+kr8865@users.noreply.github.com>
2025-09-04 11:35:10 +0530

    Create 518-CoinChangeII

---
+++ 518-CoinChangeII/version/java/518-CoinChangeII_20250904_113535.java
@@ -0,0 +1,29 @@
+// Last updated: 9/4/2025, 11:35:35 AM
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[][] dp = new int[amount+1][coins.length];
+         for(int[] i: dp){
+             Arrays.fill(i,-1);
+         }
+        return coin_exc(coins,amount,0,dp);
+        
+    }
+    public static int coin_exc(int[] coin, int amount,int i,int[][] dp){
+        if(amount==0){
+            return 1;
+        }
+        if(i==coin.length){
+            return 0;
+        }
+        int inc=0;
+        int exc=0;
+        if(dp[amount][i]!=-1){
+            return dp[amount][i];
+        }
+        if(amount>=coin[i]){
+            inc=coin_exc(coin,amount-coin[i],i,dp);
+        }
+        exc=coin_exc(coin,amount,i+1,dp);
+        return dp[amount][i]=inc+exc;
+    }
+}

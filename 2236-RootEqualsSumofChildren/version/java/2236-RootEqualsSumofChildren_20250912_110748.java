35ab6a0c2c160667362d5fdbf592af320fad76a4
kratika agrawal <misskratikaagrawal@gmail.com>
2025-09-12 11:07:43 +0530

    Create 2236-RootEqualsSumofChildren

---
+++ 2236-RootEqualsSumofChildren/version/java/2236-RootEqualsSumofChildren_20250912_110748.java
@@ -0,0 +1,25 @@
+// Last updated: 9/12/2025, 11:07:48 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean checkTree(TreeNode root) {
+        if(root.left.val+root.right.val==root.val){
+            return true;
+        }
+        return false;
+        
+    }
+}

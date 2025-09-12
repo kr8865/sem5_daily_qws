dd7ab1e93faaa250e156f84eb5367d90d03a44ca
kratika agrawal <misskratikaagrawal@gmail.com>
2025-09-12 10:55:57 +0530

    Create 205-IsomorphicStrings

---
+++ 205-IsomorphicStrings/version/java/205-IsomorphicStrings_20250912_105602.java
@@ -0,0 +1,30 @@
+// Last updated: 9/12/2025, 10:56:02 AM
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Character> mapST = new HashMap<>();
+        Map<Character, Character> mapTS = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            // check mapping s->t
+            if (mapST.containsKey(c1)) {
+                if (mapST.get(c1) != c2) return false;
+            } else {
+                mapST.put(c1, c2);
+            }
+
+            // check mapping t->s
+            if (mapTS.containsKey(c2)) {
+                if (mapTS.get(c2) != c1) return false;
+            } else {
+                mapTS.put(c2, c1);
+            }
+        }
+
+        return true;
+    }
+}

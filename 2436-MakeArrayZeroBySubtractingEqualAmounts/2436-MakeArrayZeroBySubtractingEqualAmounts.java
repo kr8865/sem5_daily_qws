9d2f88cd2226d122d6ba99266f1235d55beda50b
kratika agrawal <misskratikaagrawal@gmail.com>
2025-09-24 22:11:06 +0530

    Create 2436-MakeArrayZeroBySubtractingEqualAmounts

---
+++ 2436-MakeArrayZeroBySubtractingEqualAmounts/2436-MakeArrayZeroBySubtractingEqualAmounts.java
@@ -0,0 +1,12 @@
+// Last updated: 9/24/2025, 10:11:06 PM
+class Solution {
+    public int minimumOperations(int[] nums) {
+        HashSet<Integer> hs=new HashSet<>();
+        for(int num:nums){
+            if(num>0){
+            hs.add(num);
+            }
+        }
+        return hs.size();
+    }
+}

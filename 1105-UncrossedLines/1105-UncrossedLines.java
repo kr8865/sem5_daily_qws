bf8bddaf5e7ed59185b98c0e27554e85a9451bcf
kratika agrawal <misskratikaagrawal@gmail.com>
2025-09-24 22:12:01 +0530

    Create 1105-UncrossedLines

---
+++ 1105-UncrossedLines/1105-UncrossedLines.java
@@ -0,0 +1,27 @@
+// Last updated: 9/24/2025, 10:12:00 PM
+class Solution {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        return uncrossed(nums1,nums2);
+    }
+
+    public static int uncrossed(int[] s1, int[] s2){
+        int[][] dp = new int[s1.length+1][s2.length+1];
+        for(int i=1; i<=s1.length; i++){
+            for(int j=1; j<=s2.length; j++){
+                int ans = 0;
+
+                if(s1[i-1]==s2[j-1]){
+                    ans = 1 + dp[i-1][j-1];
+                }
+                else{
+                    int f = dp[i-1][j];
+                    int s = dp[i][j-1];
+                    ans = Math.max(f,s);
+                }
+
+                dp[i][j]=ans;
+            }
+        }
+        return dp[dp.length-1][dp[0].length-1];
+    }
+}

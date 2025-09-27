a56e974ed6c39806a6727e8ab55cbc13cd749000
kratika agrawal <misskratikaagrawal@gmail.com>
2025-09-27 14:58:03 +0530

    Create 875-KokoEatingBananas

---
+++ 875-KokoEatingBananas/version/java/875-KokoEatingBananas_20250927_145806.java
@@ -0,0 +1,35 @@
+// Last updated: 9/27/2025, 2:58:06 PM
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+        int lo=1;
+        int ans=0;
+        int hi=piles[piles.length-1];
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(caneat(mid,piles,h)){
+                ans=mid;
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        return ans;
+    }
+    public static boolean caneat(int k,int[] piles,int h){
+        int time=0;
+        for(int i=0;i<piles.length;i++){
+            if(piles[i]<=k){
+                time+=1;
+                
+            }
+            else{
+                time+=Math.ceil((double)piles[i]/(double)k);
+            }
+            
+
+        }
+        return time<=h;
+    }
+}

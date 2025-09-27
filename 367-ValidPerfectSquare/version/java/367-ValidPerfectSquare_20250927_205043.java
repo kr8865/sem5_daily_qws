489b6d1c1459b89ecc20c89dc16d34b0c024a30c
kratika agrawal <misskratikaagrawal@gmail.com>
2025-09-27 20:50:41 +0530

    Create 367-ValidPerfectSquare

---
+++ 367-ValidPerfectSquare/version/java/367-ValidPerfectSquare_20250927_205043.java
@@ -0,0 +1,29 @@
+// Last updated: 9/27/2025, 8:50:43 PM
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int low = 0;
+        int high = num;
+        int ans = 0;
+
+        while (low <= high) {
+            // Use long to avoid overflow when calculating mid * mid
+            long mid = low + (high - low) / 2;
+            long midSquared = mid * mid;
+
+            if (midSquared == num) {
+                return true; // Exact square root found
+            }
+
+            // If midSquared is less than x, move the low boundary up
+            if (midSquared < num) {
+                low = (int) mid + 1;
+                ans = (int) mid; // Update the answer with the current mid
+            } else {
+                // If midSquared is greater than x, move the high boundary down
+                high = (int) mid - 1;
+            }
+        }
+
+        return false; // Return the integer part of the square root
+    }
+}

9b3644bed02c8450eef0e887c6057addef3bf374
kratika agrawal <misskratikaagrawal@gmail.com>
2025-09-27 10:15:21 +0530

    Create 1672-RichestCustomerWealth

---
+++ 1672-RichestCustomerWealth/version/java/1672-RichestCustomerWealth_20250927_101523.java
@@ -0,0 +1,17 @@
+// Last updated: 9/27/2025, 10:15:23 AM
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max=0;
+        for(int i=0;i<accounts.length;i++){
+            int sum=0;
+            for(int j=0;j<accounts[0].length;j++){
+                sum+=accounts[i][j];
+                
+
+            }
+            max=Math.max(max,sum);
+        }
+        return max;
+        
+    }
+}

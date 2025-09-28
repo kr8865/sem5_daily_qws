d053bc4dd463fa8f637a9ebdad9e10231103f628
kratika agrawal <misskratikaagrawal@gmail.com>
2025-09-28 13:21:46 +0530

    Create 121-BestTimetoBuyandSellStock

---
+++ 121-BestTimetoBuyandSellStock/version/java/121-BestTimetoBuyandSellStock_20250928_132144.java
@@ -0,0 +1,23 @@
+// Last updated: 9/28/2025, 1:21:44 PM
+class Solution {
+    public int maxProfit(int[] prices) {
+        int khareed=Integer.MAX_VALUE;
+        int max=0;
+        int bechdia=-1;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<khareed){
+                khareed=prices[i];
+                bechdia=-1;
+            }
+            if(prices[i]>bechdia) {
+                bechdia=prices[i];
+                max=Math.max(max,(bechdia-khareed));
+
+            }
+            
+        }
+        
+        return max;
+        
+    }
+}

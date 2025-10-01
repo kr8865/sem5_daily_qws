dba93f5999667087f7a553082612f47fba1663ff
kratika agrawal <misskratikaagrawal@gmail.com>
2025-10-01 14:23:12 +0530

    Create 2410-MaximumMatchingofPlayersWithTrainers

---
+++ 2410-MaximumMatchingofPlayersWithTrainers/version/java/2410-MaximumMatchingofPlayersWithTrainers_20251001_142315.java
@@ -0,0 +1,17 @@
+// Last updated: 10/1/2025, 2:23:15 PM
+class Solution {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+        int i=0;
+        int j=0;
+        while(i<players.length && j<trainers.length){
+            if(players[i]<=trainers[j]){
+                i++;
+            }
+            j++;
+        }
+        return i;
+        
+    }
+}

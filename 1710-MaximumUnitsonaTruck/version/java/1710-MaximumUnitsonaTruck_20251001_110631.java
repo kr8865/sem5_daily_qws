703a7fc4499579110c6e8a3224db6fbaa908be80
kratika agrawal <misskratikaagrawal@gmail.com>
2025-10-01 11:06:28 +0530

    Create 1710-MaximumUnitsonaTruck

---
+++ 1710-MaximumUnitsonaTruck/version/java/1710-MaximumUnitsonaTruck_20251001_110631.java
@@ -0,0 +1,22 @@
+// Last updated: 10/1/2025, 11:06:31 AM
+class Solution {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+
+        Arrays.sort(boxTypes,(a,b)->a[1]-b[1]);
+        int c=0;
+        for(int i=boxTypes.length-1;i>=0;i--){
+            if((truckSize-boxTypes[i][0])>0){
+                c+=(boxTypes[i][1]*boxTypes[i][0]);
+                truckSize-=boxTypes[i][0];
+
+            }
+            else{
+                c+=truckSize*boxTypes[i][1];
+                truckSize=0;
+                break;
+            }
+        }
+        return c;
+        
+    }
+}

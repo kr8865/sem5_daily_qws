dfdf91f72d3178ab34b38f39f724167038dddb98
kratika agrawal <misskratikaagrawal@gmail.com>
2025-10-01 15:33:55 +0530

    Create 135-Candy

---
+++ 135-Candy/version/java/135-Candy_20251001_153358.java
@@ -0,0 +1,26 @@
+// Last updated: 10/1/2025, 3:33:58 PM
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+
+        // Left to right
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        // Right to left
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        int sum = 0;
+        for (int c : candies) sum += c;
+        return sum;
+    }
+}

0fc76a0da2a38ebd303a784300e3b2f6603e2732
kratika agrawal <misskratikaagrawal@gmail.com>
2025-10-06 11:22:53 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/version/java/70-ClimbingStairs_20251006_112255.java
@@ -0,0 +1,23 @@
+// Last updated: 10/6/2025, 11:22:55 AM
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return find(n,dp);
+        
+    }
+    public static int find(int n,int[] dp){
+        
+        if(n==1 ){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n]=find(n-1,dp)+find(n-2,dp);
+        return dp[n];
+    }
+}

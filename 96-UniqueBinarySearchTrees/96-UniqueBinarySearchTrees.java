b05fa1fac111911ba2903e4d0c376c6bf71643c8
kratika agrawal <misskratikaagrawal@gmail.com>
2025-10-17 20:06:59 +0530

    Create 96-UniqueBinarySearchTrees

---
+++ 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.java
@@ -0,0 +1,18 @@
+// Last updated: 10/17/2025, 8:07:01 PM
+class Solution {
+    public int numTrees(int n) {
+        int[] dp=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+      
+        for(int i=2;i<=n;i++){
+            for(int j=0;j<i;j++){
+                  dp[i]+=dp[j]*dp[i-j-1];
+
+            }
+          
+        }
+        return dp[n];
+        
+    }
+}

483215fe5b9538c83eca44a344b0f3798eb456fe
kratika agrawal <misskratikaagrawal@gmail.com>
2025-10-17 20:05:03 +0530

    Create 347-TopKFrequentElements

---
+++ 347-TopKFrequentElements/347-TopKFrequentElements.java
@@ -0,0 +1,43 @@
+// Last updated: 10/17/2025, 8:05:05 PM
+class Solution {
+    static class Pair implements Comparable<Pair>{
+        int val;
+        int fr;
+        Pair(int val,int fr){
+            this.val=val;
+            this.fr=fr;
+
+        }
+        @Override
+        public int compareTo(Pair p2){
+            return p2.fr-this.fr;
+        }
+    }
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        PriorityQueue<Pair> pq=new PriorityQueue<>();
+        for(int i=0;i<nums.length;i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i],1);
+            }
+            else{
+                int c=map.get(nums[i]);
+                map.put(nums[i],c+1);
+            }
+        }
+        /*for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            pq.add(new Pair(entry.getKey(), entry.getValue()));
+        } */
+       
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            pq.add(new Pair(entry.getKey(),entry.getValue()));
+            
+        }
+        int[] arr=new int[k];
+        for(int i=0;i<k;i++){
+            arr[i]=pq.remove().val;
+        }
+        return arr;
+        
+    }
+}

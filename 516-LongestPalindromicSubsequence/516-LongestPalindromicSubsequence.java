accb3ac9d1ae221213c6180f994b89e4f093447d
kratika agrawal <misskratikaagrawal@gmail.com>
2025-10-30 08:36:46 +0530

    Create 516-LongestPalindromicSubsequence

---
+++ 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.java
@@ -0,0 +1,26 @@
+// Last updated: 30/10/2025, 08:36:46
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        String s1 = new StringBuilder(s).reverse().toString();
+        int[][] dp=new int[s.length()+1][s.length()+1];
+        for(int i=0;i<=s.length();i++){
+            dp[i][0]=0;
+            dp[0][i]=0;
+
+        }
+        for(int i=1;i<=s.length();i++){
+            for(int j=1;j<=s.length();j++){
+                if(s.charAt(i-1)==s1.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    int m=dp[i][j-1];
+                    int n=dp[i-1][j];
+                    dp[i][j]=Math.max(m,n);
+                }
+            }
+        }
+        return dp[s.length()][s.length()];
+
+    }
+}

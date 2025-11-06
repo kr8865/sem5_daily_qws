f715bd75281c8b98ab33e106168aed9c827816c9
kratika agrawal <misskratikaagrawal@gmail.com>
2025-11-06 12:38:29 +0530

    Create 131-PalindromePartitioning

---
+++ 131-PalindromePartitioning/version/java/131-PalindromePartitioning_20251106_123828.java
@@ -0,0 +1,37 @@
+// Last updated: 06/11/2025, 12:38:28
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> ans=new ArrayList<>();
+		List<String> ll=new ArrayList<>();
+		partion(s,ll,ans);  
+        return ans;
+    }
+    public static void partion(String ques, List<String> ll,List<List<String>> ans){
+        if(ques.length()==0){
+        	ans.add(new ArrayList<String>(ll));
+            //System.out.println(ans);
+            return;
+        }
+        for(int cut=1;cut<=ques.length();cut++){
+            String s=ques.substring(0,cut);
+            if(ispalindrome(s)) {
+            	ll.add(s);
+            partion(ques.substring(cut),ll,ans);
+            ll.remove(ll.size()-1);
+            }
+        }
+
+	}
+    public static boolean ispalindrome(String s) {
+    	int i=0;
+    	int j=s.length()-1;
+    	while(i<j) {
+    		if(s.charAt(i)!=s.charAt(j)) {
+    			return false;
+    		}
+    		i++;
+    		j--;
+    	}
+    	return true;
+    }
+}

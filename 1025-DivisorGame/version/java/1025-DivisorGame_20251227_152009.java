401a79489f7cc17fbd8fd9d1c17cb9468d58213f
kratika agrawal <misskratikaagrawal@gmail.com>
2025-12-27 15:20:10 +0530

    Create 1025-DivisorGame

---
+++ 1025-DivisorGame/version/java/1025-DivisorGame_20251227_152009.java
@@ -0,0 +1,9 @@
+// Last updated: 27/12/2025, 15:20:09
+1class Solution {
+2    public boolean divisorGame(int n) {
+3        if(n%2==0){
+4            return true;
+5        }
+6        return false;
+7    }
+8}

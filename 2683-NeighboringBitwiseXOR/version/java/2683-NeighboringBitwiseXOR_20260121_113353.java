817a7778169ddda06e8c5ed504e07977f4998540
kratika agrawal <misskratikaagrawal@gmail.com>
2026-01-21 11:33:54 +0530

    Create 2683-NeighboringBitwiseXOR

---
+++ 2683-NeighboringBitwiseXOR/version/java/2683-NeighboringBitwiseXOR_20260121_113353.java
@@ -0,0 +1,10 @@
+// Last updated: 21/01/2026, 11:33:53
+1class Solution {
+2    public boolean doesValidArrayExist(int[] derived) {
+3        int xor=0;
+4        for(int v:derived){
+5            xor^=v;
+6        }
+7        return xor==0;
+8    }
+9}

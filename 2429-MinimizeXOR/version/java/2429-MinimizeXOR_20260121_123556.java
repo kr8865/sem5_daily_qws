3b94c1c1c70b1fa5ca02d0b9dbc71a48dd90e177
kratika agrawal <misskratikaagrawal@gmail.com>
2026-01-21 12:35:57 +0530

    Create 2429-MinimizeXOR

---
+++ 2429-MinimizeXOR/version/java/2429-MinimizeXOR_20260121_123556.java
@@ -0,0 +1,32 @@
+// Last updated: 21/01/2026, 12:35:56
+1class Solution {
+2    public int minimizeXor(int num1, int num2) {
+3        int set_bit=0;
+4        while(num2>0){
+5            num2=(num2&(num2-1));
+6            set_bit++;
+7        }
+8        int x=0;
+9        for(int i=30; i>=0;i--){
+10            int mask=(1<<i);
+11            if(( num1 & mask)!=0){
+12                x|=mask;
+13                set_bit--;
+14                if(set_bit==0){
+15                    return x;
+16                }
+17            }
+18        }
+19        for(int i=0;i<=30;i++){
+20            int mask=(1<<i);
+21            if((num1&mask)==0){
+22                x|=mask;
+23                set_bit--;
+24                if(set_bit==0){
+25                    return x;
+26                }
+27            }
+28        }
+29        return 0;
+30    }
+31}

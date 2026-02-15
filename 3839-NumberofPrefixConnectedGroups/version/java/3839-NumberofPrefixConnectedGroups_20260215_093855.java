f0ad8ce06b55f7a2b6fe6379033235a2ab53af4e
kratika agrawal <misskratikaagrawal@gmail.com>
2026-02-15 09:38:56 +0530

    Create 3839-NumberofPrefixConnectedGroups

---
+++ 3839-NumberofPrefixConnectedGroups/version/java/3839-NumberofPrefixConnectedGroups_20260215_093855.java
@@ -0,0 +1,26 @@
+// Last updated: 15/02/2026, 09:38:55
+1class Solution {
+2    public int prefixConnected(String[] words, int k) {
+3        HashMap<String,Integer> map=new HashMap<>();
+4        for(int i=0;i<words.length;i++){
+5            if(words[i].length()>=k){
+6                String prefix=words[i].substring(0,k);
+7                if(map.containsKey(prefix)){
+8                    int c=map.get(prefix);
+9                    map.put(prefix,c+1);
+10                }
+11                else{
+12                    map.put(prefix,1);
+13                }
+14            }
+15            
+16        }
+17        int a=0;
+18        for(String ans:map.keySet()){
+19            if(map.get(ans)>=2){
+20                a++;
+21            }
+22        }
+23        return a;
+24    }
+25}
